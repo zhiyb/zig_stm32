d432eb65035d3b2db0a03de9feb814e907394a00
Norman Zhi <normanzyb@gmail.com>
2024-09-29 12:25:16 +0100

    Fix atomic reads in systick

--- src/systick.zig
+++ src/systick.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const hal = @import("stm32f722.zig");
 const rcc = @import("rcc.zig");
 
@@ -9,32 +10,18 @@ pub fn config(rcc_inst: type, cfg: cfg_t) type {
     const freq_in = rcc_inst.clockHz(.AHB) / 8;
     const ratio = freq_in / cfg.tick_rate_hz;
     return struct {
-        pub var _tick: u32 = 0;
-
-        pub fn apply() void {
-            const reg = hal.SYST;
-            @atomicStore(@TypeOf(_tick), &_tick, 0, .unordered);
-
-            reg.CSR.write(.{
-                .CLKSOURCE = 0,
-                .TICKINT = 1,
-                .ENABLE = 0,
-            });
-            reg.RVR.write(.{ .RELOAD = ratio - 1 });
-            reg.CVR.write(.{ .CURRENT = 0 });
-            reg.CSR.modify(.{ .ENABLE = 1 });
-        }
+        var tick_cnt = std.atomic.Value(u32).init(0);
 
         pub fn irq() callconv(.C) void {
-            _ = @atomicRmw(@TypeOf(_tick), &_tick, .Add, 1, .monotonic);
+            _ = tick_cnt.rmw(.Add, 1, .monotonic);
         }
 
         pub fn get_tick() struct { tick: u32, cnt: u32 } {
-            var tick = @atomicLoad(@TypeOf(_tick), &_tick, .unordered);
+            var tick = tick_cnt.load(.acquire);
             var cnt: u32 = 0;
             while (true) {
-                cnt = @atomicLoad(u32, &hal.SYST.CVR.raw, .monotonic);
-                const now = @atomicLoad(@TypeOf(_tick), &_tick, .monotonic);
+                cnt = @atomicLoad(u32, &hal.SYST.CVR.raw, .acquire);
+                const now = tick_cnt.load(.monotonic);
                 if (now == tick) {
                     break;
                 } else {
@@ -47,14 +34,14 @@ pub fn config(rcc_inst: type, cfg: cfg_t) type {
         pub fn get_ms() u32 {
             if (cfg.tick_rate_hz != 1_000)
                 @compileError("Unsupported tick rate");
-            return @atomicLoad(@TypeOf(_tick), &_tick, .unordered);
+            return tick_cnt.load(.monotonic);
         }
 
         pub fn delay_ms(ms: u32) void {
             if (cfg.tick_rate_hz != 1_000)
                 @compileError("Unsupported tick rate");
-            const tick_start = @atomicLoad(@TypeOf(_tick), &_tick, .unordered);
-            while (@atomicLoad(@TypeOf(_tick), &_tick, .unordered) -% tick_start < ms) {}
+            const tick_start = tick_cnt.load(.monotonic);
+            while (tick_cnt.load(.monotonic) -% tick_start < ms) {}
         }
 
         pub fn delay_us(us: u32) void {
@@ -69,6 +56,20 @@ pub fn config(rcc_inst: type, cfg: cfg_t) type {
                 if (delta_cnt >= cnt)
                     break;
             }
+        }
+
+        pub fn apply() void {
+            const reg = hal.SYST;
+            tick_cnt.store(0, .monotonic);
+
+            reg.CSR.write(.{
+                .CLKSOURCE = 0,
+                .TICKINT = 1,
+                .ENABLE = 0,
+            });
+            reg.RVR.write(.{ .RELOAD = ratio - 1 });
+            reg.CVR.write(.{ .CURRENT = 0 });
+            reg.CSR.modify(.{ .ENABLE = 1 });
         }
     };
 }
